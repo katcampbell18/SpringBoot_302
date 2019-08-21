3372c5d1a7df5142f0f0cc037e4b48c586985e74
katcampbell18 <ktrncampbell@gmail.com>
2019-08-21 09:35:51 -0400

    app with CRUD operation

--- src/main/java/com/example/demo/HomeController.java
+++ src/main/java/com/example/demo/HomeController.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -33,6 +34,23 @@ public class HomeController {
             return "taskform";
         }
         taskRepository.save(task);
+        return "redirect:/";
+    }
+
+    @RequestMapping("detail/{id}")
+    public String showTask(@PathVariable("id") long id, Model model){
+        model.addAttribute("task",taskRepository.findById(id).get());
+        return "show";
+    }
+
+    @RequestMapping("update/{id}")
+    public String updateTask(@PathVariable("id") long id, Model model){
+        model.addAttribute("task",taskRepository.findById(id).get());
+        return "taskform";
+    }
+    @RequestMapping("delete/{id}")
+    public String delTask(@PathVariable("id") long id){
+        taskRepository.deleteById(id);
         return "redirect:/";
     }
 }
